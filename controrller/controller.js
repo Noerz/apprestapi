bf12091f381716daafdee5e1fe97903e78d29e2f
Noerz <mustaqim.pratama0@gmail.com>
2023-07-25 15:31:01 +0700

    berhasil tambah data

--- controrller/controller.js
+++ controrller/controller.js
@@ -2,6 +2,7 @@
 
 var response = require('../res');
 var connection = require('../koneksi');
+const conn = require('../koneksi');
 
 exports.index = function (req, res) {
     response.ok("Aplikasi Rest Api Berjalan", res)
@@ -21,7 +22,24 @@ exports.tampilsemuamahasiswa = function (req, res) {
 exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-        function (error,rows,fields) {
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        }
+    );
+};
+
+//Menambahkan data mahasiswa
+exports.tambahmahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)', [nim, nama, jurusan],
+        function(error,rows,fields){
             if (error) {
                 console.log(error);
             }else{
